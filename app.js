08592ee2d338d97eae92a30852afe51face9340c
josh mitchell <jshmtchll@gmail.com>
2021-05-02 11:58:38 -0500

    join for view all employees

--- app.js
+++ app.js
@@ -6,12 +6,67 @@ const figlet = require('figlet');
 const runDatabase = () => {
     return inquirer.prompt ([
         {
-            
+            type: 'list',
+            message: 'What do you need to do?',
+            name: 'start',
+            choices: ['Add an employee', 
+                    'Remove an employee', 
+                    'Update employee', 
+                    'View by department', 
+                    'View ALL employees', 
+                    'END']
         }
     ])
+    .then (data => {
+        if( data.start == 'Add an employee') {
+            addEmployee()
+        } else if (data.start == 'Remove an employee') {
+            removeEmployee()
+        } else if (data.start == 'Update employee') {
+            updateEmployee()
+            
+            
+            
+            //----------------VIEW ALL EMPLOYEES----------------------//
+        } else if (data.start == 'View ALL employees') {
+            db.query (`SELECT employees.first_name, employees.last_name, roles.title, roles.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) 
+                        AS Manager FROM employees 
+                        INNER JOIN roles on roles.id = employees.role_id 
+                        INNER JOIN department on department.id = roles.department_id 
+                        LEFT JOIN employees e on employees.manager_id = e.id;`,
+                        function (err,res) {
+                            if (err) throw err
+                            console.table(res)
+                            runDatabase()
+                        })
+                    
+            
+        }
+    })
+}
+
+function addEmployee() {
+    console.log('add employee here')
+}
+
+function removeEmployee() {
+    console.log('remove an employee')
+}
+
+function updateEmployee() {
+    console.log('update employee here')
 }
 
 
 
+runDatabase()
 
-  
+
+// validate: UserInput => {
+//     if (UserInput) {
+//       return true;
+//     } else {
+//       console.log('Please enter your Github username.');
+//       return false;
+//     }
+//   }
